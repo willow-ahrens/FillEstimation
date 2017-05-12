debef36547c96d73f6f4e0725fb5fb73b8feb0a0
Peter Ahrens <ptrahrens@gmail.com>
2017-05-12 19:34:53 -0400

    I think asx is done

--- code/asx.c
+++ code/asx.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "util.h"
 
 char *name () {
   return "asx";
 }
-
-#ifndef max
-    #define max(a,b) ((a) > (b) ? (a) : (b))
-#endif
-
-#ifndef min
-    #define min(a,b) ((a) < (b) ? (a) : (b))
-#endif
 
 /**
  *  Given an m by n CSR matrix A, estimates the fill ratio if the matrix were
@@ -62,83 +55,86 @@ int estimate_fill (size_t m,
   int Y_1[B][2 * B];
   int Y_2[B][B];
 
-  //initialize Z
-  for (int r = 0; r < 2*B; r++) {
-    for (int c = 0; c < 2*B; c++) {
-      Z[r][c] = 0;
-    }
-  }
+  size_t s = 10;
 
-  size_t i = 4;
-  size_t j = 4;
+  //Sample K items without replacement
+  s = min(s, nnz);
+  size_t samples[s];
+  size_t samples_i[s];
+  size_t samples_j[s];
+  random_choose(samples, s, 0, nnz);
 
-  //compute x for some i, j
-  for (int r = 1; r < 2*B; r++) {
-    for (int c = 1; c < 2*B; c++) {
-      Z[r][c] = 0;
-    }
-  }
-
-  for (size_t ii = max(i, B - 1) - (B - 1); ii <= min(i + (B - 1), m - 1); ii++) {
-    int r = (B + ii) - i;
-    size_t jj;
-    size_t jj_min = max(j, B - 1) - (B - 1);
-    size_t jj_max = min(j + (B - 1), n - 1);
-
-    size_t scan = ptr[ii];
-    size_t len = ptr[ii + 1] - ptr[ii];
-    size_t half;
-    size_t mid;
-    while (len > 0) {
-      half = len >> 1;
-      mid = scan + half;
-      if (ind[mid] >= jj_min) {
-        len = half;
-      } else {
-        scan = mid;
-        scan++;
-        len = len - half - 1;
+  //Create arrays of i and j
+  sort(samples, s);
+  {
+    size_t i = 0;
+    for (size_t t = 0; t < s; t++) {
+      if (ptr[i + 1] <= samples[t]) {
+        i = search_strict(ptr, i, m, samples[t]) - 1;
       }
-    }
-
-    while (scan < ptr[ii + 1] && (jj = ind[scan]) <= jj_max) {
-      int c = (B + jj) - j;
-      Z[r][c] = 1;
-      scan++;
+      samples_i[t] = i;
+      samples_j[t] = ind[samples[t]];
     }
   }
 
-  for (int r = 1; r < 2*B; r++) {
-    for (int c = 1; c < 2*B; c++) {
-      Z[r][c] += Z[r][c - 1];
-    }
-  }
+  for (size_t t = 0; t < s; t++) {
+    size_t i = samples_i[t];
+    size_t j = samples_j[t];
 
-  for (int c = 1; c < 2*B; c++) {
+    //compute x for some i, j
     for (int r = 1; r < 2*B; r++) {
-      Z[r][c] += Z[r - 1][c];
-    }
-  }
-
-  size_t fill_index = 0;
-  for (size_t b_r = 1; b_r <= B; b_r++) {
-    for (int r = B; r < B + b_r; r++) {
-      size_t o_r = (i + r + 1 - B) % b_r;
-      for (int c = 0; c < 2*B; c++) {
-        Y_1[o_r][c] = Z[r][c] - Z[r - b_r][c];
+      for (int c = 1; c < 2*B; c++) {
+        Z[r][c] = 0;
       }
     }
-    for (size_t b_c = 1; b_c <= B; b_c++) {
-      for (int c = B; c < B + b_c; c++) {
-        size_t o_c = (j + c + 1 - B) % b_c;
-        for (size_t o_r = 0; o_r < b_r; o_r++) {
-          Y_2[o_r][o_c] = Y_1[o_r][c] - Y_1[o_r][c - b_c];
+
+    for (size_t ii = max(i, B - 1) - (B - 1); ii <= min(i + (B - 1), m - 1); ii++) {
+      int r = (B + ii) - i;
+      size_t jj;
+      size_t jj_min = max(j, B - 1) - (B - 1);
+      size_t jj_max = min(j + (B - 1), n - 1);
+
+      size_t scan = search(ind, ptr[ii], ptr[ii + 1], jj_min);
+
+      while (scan < ptr[ii + 1] && (jj = ind[scan]) <= jj_max) {
+        int c = (B + jj) - j;
+        Z[r][c] = 1;
+        scan++;
+      }
+    }
+
+    for (int r = 1; r < 2*B; r++) {
+      for (int c = 1; c < 2*B; c++) {
+        Z[r][c] += Z[r][c - 1];
+      }
+    }
+
+    for (int c = 1; c < 2*B; c++) {
+      for (int r = 1; r < 2*B; r++) {
+        Z[r][c] += Z[r - 1][c];
+      }
+    }
+
+    size_t fill_index = 0;
+    for (size_t b_r = 1; b_r <= B; b_r++) {
+      for (int r = B; r < B + b_r; r++) {
+        size_t o_r = (i + r + 1 - B) % b_r;
+        for (int c = 0; c < 2*B; c++) {
+          Y_1[o_r][c] = Z[r][c] - Z[r - b_r][c];
         }
       }
-      for (size_t o_r = 0; o_r < b_r; o_r++) {
-        for (size_t o_c = 0; o_c < b_c; o_c++) {
-          fill[fill_index] += 1.0/Y_2[o_r][o_c];
-          fill_index++;
+      for (size_t b_c = 1; b_c <= B; b_c++) {
+        for (int c = B; c < B + b_c; c++) {
+          size_t o_c = (j + c + 1 - B) % b_c;
+          for (size_t o_r = 0; o_r < b_r; o_r++) {
+            Y_2[o_r][o_c] = Y_1[o_r][c] - Y_1[o_r][c - b_c];
+          }
+        }
+        for (size_t o_r = 0; o_r < b_r; o_r++) {
+          for (size_t o_c = 0; o_c < b_c; o_c++) {
+            fill[fill_index] += 1.0/Y_2[o_r][o_c];
+            fill_index++;
+          }
         }
       }
     }

e709621eca4fde1dfdc0dbbbde3df172eacbf04a
peter.ahrens <peter.ahrens@berkeley.edu>
2017-10-17 18:08:47 -0700

    next change

--- oski.sh
+++ oski.sh
@@ -5,8 +5,11 @@ export CFLAGS="-O3"
 mkdir oski_build
 mkdir oski_out
 cd oski-1.0.1h
-#autoreconf
-#automake --add-missing
+autoheader
+aclocal
+libtoolize --ltdl --copy --force
+automake --add-missing --copy
+autoconf 
 cd ..
 cd oski_build
 ../oski-1.0.1h/configure --prefix=$HOME/FillEstimation/oski_out

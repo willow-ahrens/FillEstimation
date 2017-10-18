be942893d0952bbb4e4f2f98143db72c7fdae670
peter.ahrens <peter.ahrens@berkeley.edu>
2017-10-18 10:04:51 -0700

    got some new script

--- taco.sh
+++ taco.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-mkdir taco
-MYDIR = `pwd`/taco
+mkdir -p taco
+MYDIR=`pwd`/taco
 cd taco-oopsla2017
 cmake -DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_DIR=$MYDIR .
-make -j
+make
 make install

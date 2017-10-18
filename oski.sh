bdc1b68a587dd6b078173964ad5b6d75ca67dd49
peter.ahrens <peter.ahrens@berkeley.edu>
2017-10-17 18:05:42 -0700

    yay

--- oski.sh
+++ oski.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #use GNU prgenv
+#use automake and autoconf modules
 export CFLAGS="-O3"
 mkdir oski_build
 mkdir oski_out
 cd oski-1.0.1h
-autoreconf
-automake --add-missing
+#autoreconf
+#automake --add-missing
 cd ..
 cd oski_build
 ../oski-1.0.1h/configure --prefix=$HOME/FillEstimation/oski_out

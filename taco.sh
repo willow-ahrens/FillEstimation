45a8258097d0415cfc3356dbfd24e13fc57ea0b1
peter.ahrens <peter.ahrens@berkeley.edu>
2017-10-18 10:08:32 -0700

    retrying

--- taco.sh
+++ taco.sh
@@ -2,6 +2,6 @@
 mkdir -p taco
 MYDIR=`pwd`/taco
 cd taco-oopsla2017
-cmake -DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_DIR=$MYDIR .
+cmake -DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_PREFIX=$MYDIR .
 make
 make install

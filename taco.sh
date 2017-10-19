3c979cf8e92d829c214d7414b02bb21167f9f8d1
Peter Ahrens <pahrens@lanka.csail.mit.edu>
2017-10-19 00:27:06 -0400

    should work!

--- taco.sh
+++ taco.sh
@@ -2,8 +2,7 @@
 mkdir -p taco
 mkdir -p `pwd`/taco/tmp
 MYDIR=`pwd`/taco
-#cd taco-oopsla2017
-cd taco2
+cd taco-oopsla2017
 cmake -DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_PREFIX=$MYDIR .
 make -j
 make install

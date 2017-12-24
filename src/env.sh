d17a279bcb791056409d0e7a9a6911cf03febaef
Peter Ahrens <ptrahrens@gmail.com>
2017-12-24 15:58:07 -0700

    need to use gnu make for this to work now :(

--- src/env.sh
+++ src/env.sh
@@ -1,2 +1,6 @@
-#!/bin/bash
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$1/gsl/lib:$1/taco/lib
+if [ -d "/Users/Peter/Projects/FillEstimation/deps/gsl/build/lib" ] && [[ ":$LD_LIBRARY_PATH:" != *":/Users/Peter/Projects/FillEstimation/deps/gsl/build/lib:"* ]]; then
+  export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+"$LD_LIBRARY_PATH:"}/Users/Peter/Projects/FillEstimation/deps/gsl/build/lib"
+fi
+if [ -d "/Users/Peter/Projects/FillEstimation/deps/taco/build/lib" ] && [[ ":$LD_LIBRARY_PATH:" != *":/Users/Peter/Projects/FillEstimation/deps/taco/build/lib:"* ]]; then
+  export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+"$LD_LIBRARY_PATH:"}/Users/Peter/Projects/FillEstimation/deps/taco/build/lib"
+fi

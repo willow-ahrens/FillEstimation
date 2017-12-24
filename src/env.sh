1243e229ec7646895e5b0b58f2f816be30df46de
Peter Ahrens <ptrahrens@gmail.com>
2017-12-24 16:24:44 -0700

    now we are ready to go to lanka

--- src/env.sh
+++
@@ -1,6 +0,0 @@
-if [ -d "/Users/Peter/Projects/FillEstimation/deps/gsl/build/lib" ] && [[ ":$LD_LIBRARY_PATH:" != *":/Users/Peter/Projects/FillEstimation/deps/gsl/build/lib:"* ]]; then
-  export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+"$LD_LIBRARY_PATH:"}/Users/Peter/Projects/FillEstimation/deps/gsl/build/lib"
-fi
-if [ -d "/Users/Peter/Projects/FillEstimation/deps/taco/build/lib" ] && [[ ":$LD_LIBRARY_PATH:" != *":/Users/Peter/Projects/FillEstimation/deps/taco/build/lib:"* ]]; then
-  export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+"$LD_LIBRARY_PATH:"}/Users/Peter/Projects/FillEstimation/deps/taco/build/lib"
-fi
--- src/generate_run_scripts.py
+++ src/generate_run_scripts.py
@@ -2,24 +2,25 @@ import util
 import argparse
 import os
 args = util.parse(argparse.ArgumentParser())
+prefix = "source {}; ".format(os.path.join(util.src, "env.sh"))
 
 #cache references
-util.experiment["create_script"](util.experiment["run"], "generate_references", "python {0} -e \"{1}\"".format(os.path.join(util.src, "generate_references.py"), util.read_path(args.experiment)), util.experiment["matrix_registry"].keys())
+util.experiment["create_script"](util.experiment["run"], "generate_references", prefix + "python {} -e \"{}\"".format(os.path.join(util.src, "generate_references.py"), util.read_path(args.experiment)), util.experiment["matrix_registry"].keys())
 
 #cache profile
-util.experiment["create_script"](util.experiment["run"], "generate_profile", "python {0} -e \"{1}\"".format(os.path.join(util.src, "generate_profile.py"), util.read_path(args.experiment)), [])
+util.experiment["create_script"](util.experiment["run"], "generate_profile", prefix + "python {} -e \"{}\"".format(os.path.join(util.src, "generate_profile.py"), util.read_path(args.experiment)), [])
 
 #cache spmv_times
-util.experiment["create_script"](util.experiment["run"], "generate_spmv_records", "python {0} -e \"{1}\"".format(os.path.join(util.src, "generate_spmv_records.py"), util.read_path(args.experiment)), util.experiment["matrix_registry"].keys())
+util.experiment["create_script"](util.experiment["run"], "generate_spmv_records", prefix + "python {} -e \"{}\"".format(os.path.join(util.src, "generate_spmv_records.py"), util.read_path(args.experiment)), util.experiment["matrix_registry"].keys())
 
 #make table data
 if "table_matrices" in util.experiment:
-  util.experiment["create_script"](util.experiment["run"], "generate_table_data", "python {0} -e \"{1}\"".format(os.path.join(util.src, "generate_table_data.py"), util.read_path(args.experiment)), util.experiment["table_matrices"])
+  util.experiment["create_script"](util.experiment["run"], "generate_table_data", prefix + "python {} -e \"{}\"".format(os.path.join(util.src, "generate_table_data.py"), util.read_path(args.experiment)), util.experiment["table_matrices"])
 
 #make table
 if "table_matrices" in util.experiment:
-  util.experiment["create_script"](util.experiment["run"], "generate_table", "python {0} -e \"{1}\"".format(os.path.join(util.src, "generate_table.py"), util.read_path(args.experiment)), [])
+  util.experiment["create_script"](util.experiment["run"], "generate_table", prefix + "python {} -e \"{}\"".format(os.path.join(util.src, "generate_table.py"), util.read_path(args.experiment)), [])
 
 #make plot
 if "plot_points" in util.experiment:
-  util.experiment["create_script"](util.experiment["run"], "generate_plot", "python {0} -e \"{1}\"".format(os.path.join(util.src, "generate_plot.py"), util.read_path(args.experiment)), util.experiment["plot_points"].keys())
+  util.experiment["create_script"](util.experiment["run"], "generate_plots", prefix + "python {} -e \"{}\"".format(os.path.join(util.src, "generate_plot.py"), util.read_path(args.experiment)), util.experiment["plot_points"].keys())

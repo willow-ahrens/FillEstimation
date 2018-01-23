1fd47e519e0c014420b81591e3d560c907739361
Peter Ahrens <ptrahrens@gmail.com>
2018-01-22 19:18:42 -0500

    switched to ints, and taco works!

--- src/util.h
+++ src/util.h
@@ -8,17 +8,8 @@
 
 //lo is inclusive, hi is exclusive
 
-/*
-typedef char hash_t;
-hash_t *hash_create ();
-void hash_set (hash_t *table, size_t key, size_t value);
-size_t hash_get (hash_t *table, size_t key, size_t value);
-void hash_clear (hash_t *table);
-void hash_destroy (hash_t *table);
-void random_choose (size_t *samples, size_t n, size_t lo, size_t hi);
-*/
-size_t random_range (size_t lo, size_t hi);
+int random_range (int lo, int hi);
 double random_uniform ();
-void sort (size_t *stuff, size_t n);
-size_t search (const size_t *stuff, size_t lo, size_t hi, size_t key);
-size_t search_strict (const size_t *stuff, size_t lo, size_t hi, size_t key);
+void sort (int *stuff, int n);
+int search (const int *stuff, int lo, int hi, int key);
+int search_strict (const int *stuff, int lo, int hi, int key);
